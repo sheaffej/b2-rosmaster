0da397c90e0be8cf168d6c203b7fdf5c00edfbdf
sheaffej <sheaffej@gmail.com>
2020-08-09 14:13:02 -0700

    updates from cli creation

--- run/master.sh
+++ run/master.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-DOCKER_IMAGE=ros:melodic-ros-core-bionic
+DOCKER_IMAGE="ros:melodic-ros-core-bionic"
+CONTAINER_NAME="rosmaster"
+LABEL="b2"
 ROS_PORT=11311
 
 docker run -d --rm \
---name rosmaster \
+--name ${CONTAINER_NAME} \
+--label ${LABEL} \
 --net host \
 $DOCKER_IMAGE roscore -p $ROS_PORT
